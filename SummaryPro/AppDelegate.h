bf163508bfb9236251a29188bee2a1d829964743
huweidong <407475190@qq.com>
2016-11-11 09:17:57 +0800

    Initial Commit

---
+++ SummaryPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SummaryPro
+//
+//  Created by huweidong on 11/11/16.
+//  Copyright © 2016年 huweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SummaryPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SummaryPro
+//
+//  Created by huweidong on 11/11/16.
+//  Copyright © 2016年 huweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
